98d9813659b0a956cd2e45542dc955fb9985f837
clemzouuu <92720413+clemzouuu@users.noreply.github.com>
2022-08-20 11:56:23 +0200

    first commit

---
+++ portfolio/components/Header.jsx
@@ -0,0 +1,22 @@
+export default function Header (){
+    return (
+      <header>
+        <nav className="navbar">
+          <ul className="link-group">
+            <li className="link">
+              Home
+            </li>
+            <li className="link">
+              Projets
+            </li>
+            <li className="link">
+              Curriculum
+            </li>
+            <li className="link">
+              Contact
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
